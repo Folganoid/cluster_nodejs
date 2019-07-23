3f3aaeeb5bda649465afb14b3a385b79dca6b5ee
Andrey <folganoid@yahoo.com>
2019-07-23 21:56:34 +0300

    (fnc) C

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
 const program = require('commander');
 const MasterApp = require ('./controllers/master.js');
 const SlaveApp = require ('./controllers/slave.js');
-const CONFIG = require ('./config.js');
-let mongoose = require('mongoose');
-
-mongoose.connect(
-    'mongodb://'+
-    CONFIG.mongoDbLogin + ':' +
-    CONFIG.mongoDbPassword + '@'+
-    CONFIG.mongoDbHost + ':' +
-    CONFIG.mongoDbPort + '/'+
-    CONFIG.mongoDbBase, {useNewUrlParser: true}
-);
-
-mongoose.connection.on('connected', function(){
-    console.log('Connect to Mongo successfully...');
-});
-mongoose.connection.on('error', function(){
-    console.log('Can\'t connect to mongo...');
-});
-mongoose.connection.on('disconnected', function(){
-    console.log('Mongo dicsonnected...');
-});
-mongoose.connection.close();
 
 program
     .option('-i, --index [indexCount]', 'Node index')
@@ -38,7 +16,6 @@ program
     .action(() => {
         let slave = new SlaveApp(program);
         slave.action();
-        console.log(CONFIG.mongoDbHost);
     });
 
 /**
--- controllers/slave.js
+++ controllers/slave.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const CONFIG = require ('../config.js');
+
 class SlaveApp {
 
     constructor(program) {
@@ -6,11 +9,53 @@ class SlaveApp {
         this.add = program.add;
         this.delete = program.delete;
         this.update = program.update;
-
     }
 
     action() {
-        console.log('slave', this.index);
+
+        mongoose.connect(
+            'mongodb://'+
+            CONFIG.mongoDbLogin + ':' +
+            CONFIG.mongoDbPassword + '@'+
+            CONFIG.mongoDbHost + ':' +
+            CONFIG.mongoDbPort + '/'+
+            CONFIG.mongoDbBase, {useNewUrlParser: true}
+        );
+
+        mongoose.connection.on('connected', function(){
+            console.log('Connect to Mongo successfully...');
+        });
+        mongoose.connection.on('error', function(){
+            console.log('Can\'t connect to mongo...');
+        });
+        mongoose.connection.on('disconnected', function(){
+            console.log('Mongo dicsonnected...');
+        });
+
+        const Schema = mongoose.Schema;
+
+        let rowSchema = new Schema({
+            index: Number,
+            text: String,
+            time: Date,
+        });
+
+        var SomeModel = mongoose.model('SomeModel', rowSchema );
+
+        let row = new SomeModel({
+            index: 1,
+            text: "xxx",
+            time: new Date()
+        });
+
+        row.save(function (err) {
+            if (err) {
+                console.log(err);
+            }
+
+            mongoose.connection.close();
+
+        });
     };
 }
 
